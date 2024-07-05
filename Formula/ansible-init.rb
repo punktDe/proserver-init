650eb17862e8e2d9868a5a755a8463fa977e7853
Wolfgang Medina-Erhardt <medina-erhardt@punkt.de>
2024-07-05 15:41:07 +0200

    Add homebrew formula

---
+++ Formula/ansible-init.rb
@@ -0,0 +1,14 @@
+# ansible-init.rb
+class AnsibleInit < Formula
+  desc "A Python script that facilitates setting up a Punkt.de Ansible project"
+  homepage "https://punkt.de"
+
+  head "git@git.punkt.de:pt/ansible-init.git", :using => :git, :tag => "main"
+
+  depends_on "python@3"
+
+  def install
+    bin.install "ansible-init"  # CHANGEME
+  end
+
+end
