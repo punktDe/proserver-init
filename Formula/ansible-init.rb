30445a750d43a14d424117421912ac912c470a9a
Wolfgang Medina-Erhardt <medina-erhardt@punkt.de>
2024-08-12 10:30:58 +0200

    direnv adjustments, other minor changes

--- Formula/ansible-init.rb
+++ Formula/ansible-init.rb
@@ -3,7 +3,7 @@ class AnsibleInit < Formula
   desc "A Python script that facilitates setting up a Punkt.de Ansible project"
   homepage "https://punkt.de"
 
-  head "git@git.punkt.de:pt/ansible-init.git", :using => :git, :tag => "main"
+  head "git@git.punkt.de:pt/ansible-init.git", :using => :git, :branch => "main"
 
   url "git@git.punkt.de:pt/ansible-init.git", :using => :git, :tag => "1.0.2"
 
