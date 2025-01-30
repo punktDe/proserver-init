ce466f547573ea98f850a0f1f662bb7f65f7af8a
Wolfgang Medina-Erhardt <medina-erhardt@punkt.de>
2025-01-30 15:21:09 +0100

    Fix pyproject script path, bump to 0.2.0

--- Formula/proserver-init.rb
+++ Formula/proserver-init.rb
@@ -6,7 +6,7 @@ class ProserverInit < Formula
 
   head "https://github.com/punktDe/proserver-init", :using => :git, :branch => "main"
 
-  url "https://github.com/punktDe/proserver-init", :using => :git, :tag => "0.1.0"
+  url "https://github.com/punktDe/proserver-init", :using => :git, :tag => "0.2.0"
 
   depends_on "python@3.12"
 
